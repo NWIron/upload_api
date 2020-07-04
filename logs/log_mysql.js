6a92f0f531b0fb83178e2666de2745965ff56697
Nathan W <497545532@qq.com>
2020-07-04 13:26:40 +0800

    upload file log structure complete

--- logs/log_mysql.js
+++ logs/log_mysql.js
@@ -1,34 +1,40 @@
-var mysql = require('mysql');
-
+const mysql = require('mysql');
 const pool = mysql.createPool({
     host: '10.20.2.34',
     port: '3306',
     user: 'root',
     password: 'mecn2020',
     database: 'file_log'
-})
+});
 
 module.exports = {
     file_upload_log: (req, res, next) => {
         pool.getConnection((err, connection) => {
             //Generate File Upload Logs
             if (err) {
-                console.log('Mysql Connection Error');
-            } else {
-                let addSql = 'INSERT INTO file_logs(datetime,file_name,file_path,user,action) VALUES(?,?,?,?,?)';
-                let addSqlParams = ['2020-07-01 4:00:00', 'test.txt', 'test', 'NW', 'upload'];
-
-                connection.query(addSql, addSqlParams, function (err, data) {
-                    if (err) {
-                        console.log('Mysql Query Error');
-                    }
-                    else {
-                        console.log(data);
-                        pool.end();
-                    }
-                });
+                console.log(err);
+                return
             }
+
+            let addSql = 'INSERT INTO file_logs(datetime,file_name,file_path,user,action) VALUES(?,?,?,?,?)';
+            let addSqlParams = ['2020-07-01 4:00:00', 'test.txt', 'test', 'NW', 'upload'];
+
+            //Query With Connection
+            connection.query(addSql, addSqlParams, function (error, results, fields) {
+                //Release Connection
+                connection.release();
+
+                //Error Handling
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    console.log(results);
+                }
+            });
+
         });
+
     },
     file_delete_log: (req, res, next) => {
         console.log("File Delete");
